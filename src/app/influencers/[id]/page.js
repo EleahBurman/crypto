14491a248f2a74ea2700edf3ac4f4ca4be8b4e90
eleahburman <eleahjburman@gmail.com>
2024-05-24 11:20:53 -0400

    Front end fetches for backend id

--- src/app/influencers/[id]/page.js
+++ src/app/influencers/[id]/page.js
@@ -1,9 +1,39 @@
 "use client"
 
 import { useParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
 export default function Influencer(){
-  const params = useParams()
-  console.log(params.id)
+  // const [id, setId]= useState(undefined);
+  const params = useParams();
+  
+  useEffect(()=>{
+    const init = async () => {
+      try{
+        const response = await fetch(
+          "/influencers/api",
+          {
+            method: "GET",
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({id})
+          }
+        )
+        const res = await response.json();
+        if(response.ok){
+          setFeedback({status: "success", message: res.message})
+        }else{
+          setFeedback({status: "danger", message: res.message})
+        }
+      }catch(e){
+        setFeedback({status: "danger", message: e.message})
+      }
+    }
+    init();
+  }, [params.id]);
+
   return(
     <div>Hi, I am an influencer</div>
   )
